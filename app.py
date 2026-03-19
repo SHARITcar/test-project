925838f11daa7ad5e28571be3f6ec25d2c769064
Thomas Lubbers <thomaslubbers@Thomass-MacBook-Pro.local>
2026-03-19 17:16:21 +1100

    Set login page as root landing page

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def password_reset():
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
